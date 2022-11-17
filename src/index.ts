ea96045a68e4b126d43b096297320e2027ec09cd
Ryan Omasta <ryand@emeraldsys.xyz>
2022-11-17 12:37:00 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import {
     S3Client,
     PutObjectCommandInput,
     PutObjectCommand,
-    PutObjectCommandOutput,
-    
+    PutObjectCommandOutput
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import * as fs from "fs";
