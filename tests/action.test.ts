10e87b465616fd3ea1ca6191cb6763401206e9ad
Ryan <ryand@emeraldsys.xyz>
2023-08-10 13:56:40 -0600

    fix(action/run): Fix nested folders and default to source dir when destination dir is undefined (#272)

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -8,7 +8,7 @@ describe("r2-upload-action", () => {
         ? RunTarget.mainJs("action.yml")
         : RunTarget.jsFile("dist/index.js", "action.yml");
 
-    it("Upload test file to root of R2", async () => {
+    it("Upload test directory to root of R2", async () => {
         const options = RunOptions.create({
             inputs: {
                 "r2-account-id": process.env.TEST_R2_ACCOUNT_ID,
@@ -16,15 +16,11 @@ describe("r2-upload-action", () => {
                 "r2-secret-access-key": process.env.TEST_R2_SECRET_AK,
                 "r2-bucket": process.env.TEST_R2_BUCKET,
                 "source-dir": "tests/dir/",
-                "destination-dir": "hello/"
+                "destination-dir": "./"
             }
         });
         const res = await target.run(options);
 
         expect(res.isSuccess).toEqual(true);
     });
-
-    /* it("Upload test directory to root of R2", async () => {
-        
-    }); */
 });
