e12ce596e1a07b5c70606719c0dfed714e8e4a20
Ryan Omasta <git@ryand.ca>
2024-07-31 00:05:27 +0000

    feat: add nix shell check (#435)

--- shell.nix
+++ shell.nix
@@ -1,7 +1,26 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [
+{
+  system ? builtins.currentSystem,
+}:
+let
+  nixpkgs = fetchTarball {
+    url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    sha256 = "1aabz2zbbf2l6b85v04cc64ff8hfp09g3hnj41iq45kcpb9r3qmx";
+  };
+
+  pkgs = import nixpkgs {
+    inherit system;
+    config = { };
+    overlays = [ ];
+  };
+in
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # Format using nixfmt
+    nixfmt-rfc-style
+
+    node2nix
     nodejs
     nodePackages.pnpm
+    yarn
   ];
 }
