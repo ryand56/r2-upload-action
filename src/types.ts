4e203febd2544670291d79c5ef8463cc540ea5e8
Ryan Omasta <git@ryand.ca>
2024-07-31 00:22:39 +0000

    Release v1.3 (#436)
    Co-authored-by: r2-action-bot[bot] <118486773+r2-action-bot[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Omasta <git@ryand.ca>
    Co-authored-by: Eric Lam <tsukiko852@gmail.com>
    Co-authored-by: 叶志晟 <yzs981130@126.com>

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,23 @@ export interface R2Config {
     accessKeyId: string
     secretAccessKey: string
     bucket: string
+    jurisdiction: string
     sourceDir: string
     destinationDir: string
     outputFileUrl: boolean
+    multiPartSize: number
+    maxTries: number
+    multiPartConcurrent: boolean
     keepFileFresh: boolean
 }
 
 export interface FileMap {
     [file: string]: string
 }
+
+export interface UploadResult<T extends object> {
+    output: T
+    url: string
+}
+
+export type UploadHandler<T extends object> = (file: string, config: R2Config) => Promise<UploadResult<T>>
